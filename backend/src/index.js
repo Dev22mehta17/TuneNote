d68cd1aea930bf68b0e4f57eb070ecb12302c911
dev mehta <mehtadev2004@gmail.com>
2025-07-10 03:07:19 +0530

    SFdf

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+// import cors from 'cors';
+
+// app.use(cors());
+
 
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT;
 
-app.listen(5000,()=>{
+app.use("/api/users",userRoutes);
+
+app.listen(3000,()=>{
     console.log("Server is running on port "+PORT);
 })
