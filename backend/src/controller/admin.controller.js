1d927d02c8fc2d902a529393dcfe539bb9fedef1
dev mehta <mehtadev2004@gmail.com>
2025-07-10 22:10:05 +0530

    asdadf

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,3 +1,33 @@
- export const createSong =(req,res)=>{
-    res.send("Admin route with GET method");
-}
+import { Song } from "../models/song.model.js";
+import { Album } from "../models/album.model";
+ export const createSong =async (req,res,next)=>{
+    try{
+        if(!req.files || !req.files.audioFile || !req.files.imageFile){
+            return res.status(400).json({message:"Please Upload all files"});
+        }
+        const (title,artist,albumId,duration)=req.body
+        const audioFile=req.files.audioFile
+        const imageFile=re.files.imageFile
+
+        const song =new Song({
+            title,
+            artist,
+            audioUrl,
+            imageUrl,
+            duration,
+            albumId: albumId ||null
+        })
+        await song.save()
+        if(albumId){
+            await Album.findByIdAndUpdate(albumId,{
+                $push:{songs:song._id},
+            })
+        }
+        res.status(201).json(song)
+    }
+    catch(error){
+        console.log("Error in CreateSong ",error);
+        next(error);
+
+    }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from '@clerk/express'
+import fileUpload from "express-fileupload"
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
@@ -16,12 +18,23 @@ import statRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
+const __dirname=path.resolve();
 const app=express();
 const PORT=process.env.PORT; 
 
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:path.join(__dirname,"tmp"),
+    createParentPath:true,
+    limits:{
+        fileSize:10*1024*1024,
+    },
+
+}));
+
 
 app.use("/api/users",userRoutes);
 app.use("/api/admin",adminRoutes);
@@ -29,6 +42,10 @@ app.use("/api/auth",authRoutes);
 app.use("/api/songs",songRoutes);
 app.use("/api/albums",albumRoutes);
 app.use("/api/stats",statRoutes);
+
+app.use((err,req,res,next)=>{
+    res.status(500).json({message:process.env.NODE_ENV==="production"?"Internal Server Error":err.message});
+})
 
 app.listen(3000,()=>{
     console.log("Server is running on port "+PORT);
