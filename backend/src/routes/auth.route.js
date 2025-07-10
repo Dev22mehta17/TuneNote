400a73bc9d92a7b7746ce7a203ca9828bbd042ac
dev mehta <mehtadev2004@gmail.com>
2025-07-10 12:54:49 +0530

    jasm

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,24 +1,8 @@
 import { Router } from "express";
-import { User } from "../models/user.model.js";
+import { authCallback } from "../controller/auth.controller.js";
+// import { User } from "../models/user.model.js";
 
 const router=Router()
-router.post("/callback", async(req,res)=>{
-    try{
-        const {id,firstName,lastName,imageUrl}=req.body;
-        const user=await User.findOne({clerkId:id});
-
-        if(!user){
-            await User.create({
-                clerkId: id,
-                fullName: `${firstName} ${lastName}`,
-                imageUrl
-            })
-        }
-        res.status(200).json({success:true})
-    }catch(error){
-        console.log("Error in auth callback",error);
-        res.status(500).json({message:"Internal server error",error});
-    }
-})
+router.post("/callback", authCallback)
 
 export default router;
