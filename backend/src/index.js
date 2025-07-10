882df91ccdf97b85aca3e91c0f31e73328899b4a
dev mehta <mehtadev2004@gmail.com>
2025-07-10 11:26:58 +0530

    ygkjh

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
-// import cors from 'cors';
+import adminRoutes from "./routes/admin.route.js";
+import authRoutes from "./routes/auth.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
+
 
 // app.use(cors());
 
@@ -9,9 +14,14 @@ import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 const app=express();
-const PORT=process.env.PORT;
+const PORT=process.env.PORT; 
 
 app.use("/api/users",userRoutes);
+app.use("/api/admin",adminRoutes);
+app.use("/api/auth",authRoutes);
+app.use("/api/songs",songRoutes);
+app.use("/api/albums",albumRoutes);
+app.use("/api/stats",statRoutes);
 
 app.listen(3000,()=>{
     console.log("Server is running on port "+PORT);
