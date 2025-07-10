2146de39e1630db7f4c3d952894e2ba0ffe080c6
dev mehta <mehtadev2004@gmail.com>
2025-07-10 12:02:31 +0530

    jhbadj

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
+import { getAdmin } from "../controller/admin.controller";
 
 const router=Router()
-router.get("/",(req,res)=>{
-    res.send("Admin route with GET method");
-})
+router.get("/",getAdmin)
 
 export default router;
