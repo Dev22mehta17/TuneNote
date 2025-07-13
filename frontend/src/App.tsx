39a5aec7ff2f759961adeb0489c4ef31a327d6af
dev mehta <mehtadev2004@gmail.com>
2025-07-13 18:13:09 +0530

    asknd,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 
 
-import {Routes} from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
 
   return (
     <>
      <Routes>
-
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
      </Routes>
     </>
   )
