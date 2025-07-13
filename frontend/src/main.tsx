64b834dcff0953050d240a62793b750c625f3b61
dev mehta <mehtadev2004@gmail.com>
2025-07-13 18:03:22 +0530

    ad

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,6 +15,8 @@ import { ClerkProvider } from '@clerk/clerk-react';
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+
 
 import App from './App'
 import './index.css' // ✅ Important
@@ -29,7 +31,10 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
+     
     </ClerkProvider>
   </React.StrictMode>,
 )
